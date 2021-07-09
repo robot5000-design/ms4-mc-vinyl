e044302c4885980241f8e04edbaa68bb6bc10458
robot5000-design <ducativtwin@gmail.com>
2021-07-09 22:32:37 +0000

    Remove unneccessary overlays

--- cart/templatetags/cart_tools.py
+++ cart/templatetags/cart_tools.py
@@ -2,6 +2,12 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
+    """ Calculates price * quantity
+
+    Returns:
+        price * quantity
+    """
     return price * quantity
